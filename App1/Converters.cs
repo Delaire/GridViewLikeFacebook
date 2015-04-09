728d5d3140ac69945274752d76cc56dddb457ca7
Damien Delaire <delairedd@gmail.com>
2015-04-09 15:00:09 -0700

    First Commit

---
+++ App1/Converters.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace App1
+{
+    public class ScrollToMarginConverter : IValueConverter
+    {
+        public double OldScrollPosition { get; set; }
+        
+        //this will allow you to ease in the top bar and ease it out
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var scrollPosition = (double)value;
+            var maxHeight = System.Convert.ToInt32(parameter)+60;
+
+            // to hide the navigation ar when the user first starts scrolling
+            var topMargin = -scrollPosition / 2;
+
+            //saving Value
+            //if OldPosition is inferiour to scrollPosition then user is scroll backwards (up)
+            //Scrolling up
+            if (OldScrollPosition > scrollPosition)
+            {
+                var newTopMargin = ((OldScrollPosition - scrollPosition) / 2) - maxHeight;
+
+                topMargin = Math.Min(0, newTopMargin);
+            }
+            else
+            {
+                //Scrolling down
+
+                //saving old position to check to see when whe scroll up
+                OldScrollPosition = scrollPosition;
+                topMargin = Math.Max(-maxHeight, topMargin);
+            }
+
+           
+            //Debug.WriteLine("H: {0}", topMargin);
+            //Debug.WriteLine("S: {0}", scrollPosition);
+
+            return new Thickness(0, topMargin, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //public class ScrollToOpacityConverter : IValueConverter
+    //{
+    //    public double OldPosition { get; set; }
+    //    public object Convert(object value, Type targetType, object parameter, string language)
+    //    {
+
+    //        var scrollPosition = ((double)value);
+    //        var scrollNow = scrollPosition / 2;
+    //        var maxHeight = System.Convert.ToInt32(parameter); ;
+    //        var opacity = Math.Min(maxHeight, scrollNow) / maxHeight;
+          
+    //        if (OldPosition > scrollPosition)
+    //        {
+    //            var newOpacity = ((scrollPosition - OldPosition)) / maxHeight;
+
+    //            opacity = Math.Min(1, newOpacity);
+    //        }
+    //        else
+    //        {
+    //            //Scrolling down
+
+    //            //saving old position to check to see when whe scroll up
+    //            OldPosition = scrollPosition;
+    //            //topMargin = Math.Max(-maxHeight, topMargin);
+    //        }
+
+    //        if (opacity < 0.01) opacity = 0;
+
+    //        Debug.WriteLine("S: {0}", scrollNow);
+    //        Debug.WriteLine("O: {0}", opacity);
+
+    //        return (1 - opacity);
+    //    }
+
+    //    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+}
---
+++ App1/FriendlyListView.cs
@@ -0,0 +1,63 @@
+﻿using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using System.Windows;
+ 
+using Windows.UI.Xaml.Input;
+
+namespace App1
+{
+    public class FriendlyListView : GridView
+    {
+        private ScrollViewer _viewport;
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _viewport = (ScrollViewer)GetTemplateChild("ScrollViewer");
+            _viewport.ViewChanged += OnViewportChanged;
+        }
+
+       
+
+        public double ScrollPosition
+        {
+            get { return (double)GetValue(ViewPortProperty); }
+            set { SetValue(ViewPortProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewPortProperty = DependencyProperty.Register(
+            "ScrollPosition",
+            typeof(double),
+            typeof(FriendlyListView),
+            new PropertyMetadata(0d, OnViewPortChanged));
+
+
+        private void OnViewportChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            //ScrollPosition = _viewport.Viewport.Top;
+            ScrollPosition = _viewport.VerticalOffset;
+        }
+
+        private static void OnViewPortChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var list = (FriendlyListView)d;
+
+            //if (list._viewport.Viewport.Top.Equals(list.ScrollPosition)) return;
+
+            //if (list._viewport.ManipulationState == ManipulationState.Idle)
+            //{
+            //    list._viewport.SetViewportOrigin(new Point(0, list.ScrollPosition));
+            //}
+
+            if (list._viewport.ViewportHeight.Equals(list.ScrollPosition)) return;
+
+            if (list._viewport.ManipulationMode == ManipulationModes.None)
+            {
+                //list._viewport.SetValue(new Point(0, list.ScrollPosition));
+                //list._viewport.SetViewportOrigin(new Point(0, list.ScrollPosition));
+            }
+        }
+    }
+}
